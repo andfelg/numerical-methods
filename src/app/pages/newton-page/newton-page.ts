b22b201486ba56876f15e5fd49841c61be866347
Daniela <mina454f@gmail.com>
2026-06-08 21:31:36 -0500

    feat: newton visuals

--- src/app/pages/newton-page/newton-page.ts
+++ src/app/pages/newton-page/newton-page.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
 import { BackButton } from '../../components/back-button/back-button';
+import { Loadindicator } from '../../components/loadindicator/loadindicator';
+import { FormsModule } from '@angular/forms';
+
+export interface FormValues {
+  func: string;
+  interA: number | null;
+  interB: number | null;
+  maxIterations: number | null;
+  tolerance: number | null;
+}
 
 @Component({
   selector: 'app-newton-page',
-  imports: [BackButton],
+  imports: [BackButton, Loadindicator, FormsModule],
   templateUrl: './newton-page.html',
 })
-export class NewtonPage {}
+export class NewtonPage {
+  isWaiting: boolean = true;
+
+  formValues: FormValues = {
+      func: '',
+      interA: null,
+      interB: null,
+      maxIterations: null,
+      tolerance: null
+    };
+
+  iniciarMetodo(){
+
+  }
+
+  siguiente(){
+
+  }
+
+  anterior(){
+
+  }
+
+  limpiarFormulario(){}
+}
